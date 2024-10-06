d7b3c33d448b0baf21d145fa203c88b82933ba15
SREEJIT BAKSHI <SREEJIT BAKSHI@LAPTOP-LO04BSGJ>
2024-10-06 15:28:13 +0530

    Commit by Sreejit

--- src/java/com/vit/dao/UserDAO.java
+++ src/java/com/vit/dao/UserDAO.java
@@ -1,85 +1,81 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.vit.dao;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.vit.pojo.User;
+import java.sql.*;
+import java.util.ArrayList;
 
 /**
  *
  * @author SREEJIT BAKSHI
  */
-public class UserDAO extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UserDAO</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UserDAO at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+public class UserDAO {
+    public Connection con = null;
+    public Statement stmt = null;
+    public ResultSet rs=null;
+    public PreparedStatement pStmt=null;
+    
+    public static UserDAO instance=null;
+    
+    public static synchronized UserDAO getInstance()
+    {
+        if (instance == null)
+            instance = new UserDAO();
+ 
+        return instance;
+    }
+    private UserDAO() {
+        try{            
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres", "12345");
+            System.out.println("Connection Object--------->"+con);
+            stmt=con.createStatement();
+        }
+        catch(Exception e){            
+            e.printStackTrace();            
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    
+    public ArrayList executeSearch(String searchString){
+        ArrayList userList = new ArrayList();
+        try{
+            System.out.println("SEARCH"+searchString);
+            rs= stmt.executeQuery("select * from \"userdata\" "
+                    + "where uname like'%"+searchString+"%'");
+            while(rs.next()){
+                User user = new User(
+                    rs.getString(1), rs.getString(2), rs.getString(4),rs.getString(5),
+                    rs.getInt(3));
+                userList.add(user);
+            }         
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return userList;
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    
+    public boolean executeInsert(User user){
+        try{
+            pStmt = con.prepareStatement("insert into \"userdata\" values(?,?,?,?,?)");
+            pStmt.setString(1,user.getUserId());
+            pStmt.setString(2,user.getUserName());
+            pStmt.setString(4,user.getEmailId());
+            pStmt.setString(5,user.getPassword());
+            pStmt.setInt(3,user.getAge());
+            int rowCount= pStmt.executeUpdate();
+            if(rowCount >0){
+                return true;
+            }
+            
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return false;
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }
